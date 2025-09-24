2946592978aedeae3c7252c9dfe12866e6f2af84
Marvelous <ezekemarvelous@gmail.com>
2025-09-24 18:13:45 +0100

    work done

--- student_score.py
+++ student_score.py
@@ -11,6 +11,7 @@ def result():
     if request.method == "POST":
         result = request.form
         return render_template("result_page.html", result=result)
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
